848b0b1d8818ec008076cd66e8f203a991e1f5eb
SanjanaMadupoju <123532534+SanjanaMadupoju@users.noreply.github.com>
2023-02-01 10:07:07 +0530

    Add files via upload

---
+++ ConstDemo.js
@@ -0,0 +1,13 @@
+const pi=3.14;
+//pi=10;
+//pi++;
+for(let i=1;i<6;i++){
+   const a=pi*i*i;
+   console.log("area ="+a);
+}
+const r=10;
+if(pi==3.14){
+    const r=12;
+    console.log("inside if "+r);
+}
+console.log("outside "+r);
